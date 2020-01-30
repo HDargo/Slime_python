0e96310f2e8644218389b06f4110a49c3f2ea172
Edge903 <blasolgd5901@gmail.com>
2020-01-30 18:03:04 +0900

    E

--- Taro/spread/one_card_spread.py
+++ Taro/spread/one_card_spread.py
@@ -3,9 +3,16 @@ from Taro.spread.spread_interface import spread_interface
 
 class spread(spread_interface):
     def OptionPrint(self):
+        self.deck.Generator()
+        self.deck.shuffle()
         print("=====================================================================")
         print("0 ~ 21 까지의 카드 중 1개를 선택 해 주세요.")
         print("그 외 숫자를 입력하시면 취소됩니다")
         print("=====================================================================")
-        num = int(input("Choice : "))
-        print(self.deck.list[num].Name)
+        temp = input("Choice : ")
+        Opt = self.Optimization(temp)
+        if len(Opt) != 1:
+            print("오류!")
+            self.OptionPrint()
+        else:
+            print(self.deck.list[Opt].Name)
