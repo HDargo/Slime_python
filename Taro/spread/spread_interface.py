e194cdd87eacfab862d32a99bac70e0168d0dd44
HDargo <argo001@naver.com>
2020-01-30 13:54:17 +0900

    main

--- Taro/spread/spread_interface.py
+++ Taro/spread/spread_interface.py
@@ -3,8 +3,10 @@ from Taro.Taro_Deck.taro_deck import Taro_deck as t
 
 class spread_interface:
     def __init__(self):
-        self.deck = []
+        self.deck = t()
+
     def OptionPrint(self):
         raise NotImplementedError()
+
     def Generator(self):
-        self.deck = t().Generator()
+        self.deck = t()
