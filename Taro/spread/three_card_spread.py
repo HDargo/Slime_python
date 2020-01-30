3d58ae899dc1b6b5f6ec7307e8d465c2675d49eb
player138 <54841072+player138@users.noreply.github.com>
2020-01-30 16:25:10 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Taro/spread/three_card_spread.py

--- Taro/spread/three_card_spread.py
+++ Taro/spread/three_card_spread.py
@@ -1,5 +1,4 @@
 from Taro.spread.spread_interface import spread_interface
-from Taro.Taro_Deck.taro_deck import Taro_deck as shuffle
 
 class spread(spread_interface):
     def OptionPrint(self):
@@ -9,5 +8,7 @@ class spread(spread_interface):
         print("0 ~ 21 까지의 카드 중 1개를 선택 해 주세요.")
         print("그 외 숫자를 입력하시면 취소됩니다")
         print("=====================================================================")
-        num = int(input("Choice : "))
-        print(self.deck.list[num].Name)
+        temp = str(input("Choice : "))
+        num = temp.split()
+        for x in num
+            print(self.deck.list[int(num)].Name)
