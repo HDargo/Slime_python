a9eca82c58f29158118eb80e7651c13949b1a2b3
HDargo <argo001@naver.com>
2020-01-30 21:33:52 +0900

    Old maid

--- Old_maid/character/ai.py
+++ Old_maid/character/ai.py
@@ -2,8 +2,5 @@ class ai:
     def __init__(self):
         self.deck = []
 
-    def print(self):
-        print("ai test")
-
     def card_append(self, card):
         self.deck.append(card)
--- Old_maid/character/player.py
+++ Old_maid/character/player.py
@@ -2,8 +2,5 @@ class player:
     def __init__(self):
         self.deck = []
 
-    def print(self):
-        print("player test")
-
     def card_append(self, card):
         self.deck.append(card)
