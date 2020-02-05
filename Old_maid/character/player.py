8ac20931622445c89ed627ef2a6503c9cec9426f
jaeho12347 <jaeho12347@gmail.com>
2020-02-05 22:30:07 +0900

    deck

--- Old_maid/character/player.py
+++ Old_maid/character/player.py
@@ -5,9 +5,32 @@ class player:
 
     def card_append(self, card):
         self.deck.append(card)
+        print(self.deck)
+
 
     def card_select(self, next_list_len):
-        pass
+
+
+        next_list_len()
+
+
 
     def deck_delete(self):
-        pass
+        le = len(self.deck)
+        for a in range(le):
+            for b in range(le):
+                print("a = ", a)
+                print("b = ", b)
+                print("le = ", le)
+                print("==========")
+                if a == b:
+                    break
+                elif deck[a][1:] == self.deck[b][1:]:
+                    print("중복제거")
+                    if a > b:
+                        self.deck.remove(self.deck[a])
+                        self.deck.remove(self.deck[b])
+                    if a < b:
+                        self.deck.remove(self.deck[b])
+                        self.deck.remove(self.deck[a])
+                    le -= 2
