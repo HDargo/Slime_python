519af58a7afd240a0c18cfb375733274e53559e2
jaeho12347 <jaeho12347@gmail.com>
2020-02-09 00:03:24 +0900

    deck

--- Old_maid/character/player.py
+++ Old_maid/character/player.py
@@ -16,21 +16,25 @@ class player:
 
 
     def deck_delete(self):
-        le = len(self.deck)
-        for a in range(le):
-            for b in range(le):
-                print("a = ", a)
-                print("b = ", b)
-                print("le = ", le)
-                print("==========")
-                if a == b:
-                    break
-                elif deck[a][1:] == self.deck[b][1:]:
-                    print("중복제거")
-                    if a > b:
-                        self.deck.remove(self.deck[a])
-                        self.deck.remove(self.deck[b])
-                    if a < b:
-                        self.deck.remove(self.deck[b])
-                        self.deck.remove(self.deck[a])
-                    le -= 2
+        try:
+            le = len(self.deck)
+
+            for a in range(le):
+                for b in range(le):
+                    print("a = ", a)
+                    print("b = ", b)
+                    print("le = ", le)
+                    print("==========")
+                    if a == b:
+                        break
+                    elif self.deck[a][1:] == self.deck[b][1:]:
+                        print("중복제거")
+                        if a > b:
+                            self.deck.remove(self.deck[a])
+                            self.deck.remove(self.deck[b])
+                        if a < b:
+                            self.deck.remove(self.deck[b])
+                            self.deck.remove(self.deck[a])
+                        le -= 2
+        except IndexError:
+            print('a')
