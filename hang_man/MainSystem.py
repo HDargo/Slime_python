c64f0176f5c208443ed1be77b32051d90b8ff7c6
jaeho12347 <jaeho12347@gmail.com>
2020-02-19 22:30:58 +0900

    deck

--- hang_man/MainSystem.py
+++ hang_man/MainSystem.py
@@ -5,7 +5,7 @@ from hang_man.player_word.word import wordsel
 class hamg_man:
 
     def __init__(self):
-        self.pl = player()
+        self.chance = 7
         self.li = player()
         self.question = ""
         self.word = wordsel()  # 랜덤  문자  받기
@@ -15,11 +15,15 @@ class hamg_man:
     def questionMethod(self):
         for x in range(len(self.word)):
             self.question += "_ "
-        print(self.pl.answer)
-
+        print(self.li.answer)
+        self.temp()
 
     def temp(self):
-        self.pl.check(self.word)
+        self.li.check(self.word)
+        self.questionMethod()
+        while self.chance != 0:
+            self.chance -= 1
+            pass
 
     def quest(self):
         pass
@@ -29,5 +33,3 @@ if __name__ == "__main__":
     a = hamg_man()
     print(a.word)
     print(a.question)
-    a.temp()
-    a.questionMethod()
--- hang_man/player_word/player.py
+++ hang_man/player_word/player.py
@@ -3,21 +3,22 @@ class player:
     def __init__(self):
         self.answer = []
 
-    def playerlist(self,aaa):
-        for x in range(len(aaa)):
-            self.answer += ["_"," "]
+    def playerlist(self,word_len):
+        for x in range(len(word_len)):
+            self.answer.append("_")
         print(self.answer)
 
     def check(self,test):
         ans = input()
+
         if ans in test:
-            print("True")
             for x in range(len(test)):
                 print(test[x])
 
         else:
             print("틀렸습니다")
-        pass
+            self.check(test)
+
     #플레이어에서 답 입력 함수 실행 후 정답과 비교
     #정답일 시 플레이어 클래스에 있는 정답 리스트에 추가
     #오답일 시 넘어감
