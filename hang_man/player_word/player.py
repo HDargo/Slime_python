b0407e3ec8b5accd6fd83a120b46333d94f7d3ae
jaeho12347 <jaeho12347@gmail.com>
2020-02-20 21:40:05 +0900

    deck

--- hang_man/player_word/player.py
+++ hang_man/player_word/player.py
@@ -17,16 +17,18 @@ class player:
         if ans in test:
             for x in range(len(test)):
                 print(test[x])
-        if self.chance == 0:
-            print("실패")
-            sys.exit
-
 
         else:
             print("틀렸습니다")
             self.chance -= 1
-            print(self.chance)
+            if self.chance == 0:
+                print("실패")
+                sys.exit()
+            print("남은기회:",self.chance)
             self.check(test)
+
+
+
 
     #플레이어에서 답 입력 함수 실행 후 정답과 비교
     #정답일 시 플레이어 클래스에 있는 정답 리스트에 추가
