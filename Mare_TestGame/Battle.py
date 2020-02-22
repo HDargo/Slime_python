b89762ffd615b8a1c4758bd1df804e48df68114e
jaeho12347 <jaeho12347@gmail.com>
2020-02-22 22:40:53 +0900

    deck

--- Mare_TestGame/Battle.py
+++ Mare_TestGame/Battle.py
@@ -1,4 +1,13 @@
-from
+from Mare_TestGame.Field import field
 
 class fight:
-    pass
+    def __init__(self):
+        print(self," 와 조우 했다!")
+        self.do()
+        pass
+
+    def do (self):
+        print("가능한 행동")
+
+    def reward(self):
+        pass
---
+++ Mare_TestGame/Player.py
@@ -0,0 +1,11 @@
+class player:
+    def __init__(self):
+        if self.new == 0:
+            print("기본 아이템 지급")
+        elif self.new == 1:
+            pass
+        else:
+            print("뭔가 잘못됬다")
+        pass
+
+    def
