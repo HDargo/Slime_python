cf7b92f8bb5925621baf2cf30ebaf2b002ae78e2
jaeho12347 <jaeho12347@gmail.com>
2020-02-23 03:11:38 +0900

    deck

--- Mare_TestGame/Player.py
+++ Mare_TestGame/Player.py
@@ -1,22 +1,28 @@
 class player:
     def __init__(self):
-        self.new = 0
-        if self.new == 0:
+        print("[1]새로운 시작")
+        print("[2]이어하기")
+        self.new = int(input())
+        if self.new == 1:
             print("기본 아이템 지급")
-            self.name = input()
+            self.name = input("나의...이름은...")
             self.hp = 100
-            self.maxHp = 100
-            self.power = 5
-            self.armor = 0
-        elif self.new == 1:
+            self.maxhp = 100
+            self.power = 1
+            self.armor = 100
+            self.money = 50
+            self.inven = ["시작의 검","허름한 가죽옷","빨간포션"]
+        elif self.new == 2:
             pass
 
+
     def status(self):
-        print(self.name)
-        print(self.hp)
-        print(self.maxHp)
-        print(self.power)
-        print(self.armor)
+        print("내이름 =",self.name)
+        print("현재 체력 =",self.hp)
+        print("최대 체력 =",self.maxhp)
+        print("공격력 =",self.power)
+        print("방어력 =",self.armor)
+        print("소지금 =",self.money)
 
     def item(self):
-        pass
+        print(self.inven)
--- Mare_TestGame/TestGame.py
+++ Mare_TestGame/TestGame.py
@@ -6,7 +6,33 @@ from Mare_TestGame.Player import player
 class mainsys:
     def __init__(self):
         self.pla = player()
-        pass
+        self.whatdo()
+
+    def whatdo(self):
+        while True:
+            try:
+                print("무엇을할까?")
+                print("[1] 상점가기")
+                print("[2] 휴식")
+                print("[3] 모험!")
+                print("[4] 내상태")
+                print("[5] 저장하기")
+                do = int(input())
+
+                if do == 1:
+                    self.shop()
+                elif do == 2:
+                    self.heal()
+                elif do == 3:
+                    self.fight()
+                elif do == 4:
+                    self.statuspr()
+                elif do == 5:
+                    self.save()
+                else:
+                    print("숫자를 잘못 골랐습니다")
+            except:
+                print("오류 발생 다시입력해주세요")
 
     def shop(self):
         pass
@@ -15,7 +41,7 @@ class mainsys:
         pass
 
     def fight(self):
-        fight()
+        fight().do(self.pla)
 
     def statuspr(self):
         print("[1] 상태보기")
@@ -26,14 +52,12 @@ class mainsys:
             self.pla.status()
         if choice == 2:
             self.pla.item()
+        self.whatdo()
 
     def save(self):
         pass
 
 
-mainsys().statuspr()
+mainsys()
 
 
-class weapon:
-    def __init__(self):
-        pass
