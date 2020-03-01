d7952dd7dd849893b2cd2bb1b614b7ac3e9d9867
jaeho12347 <jaeho12347@gmail.com>
2020-03-01 19:33:15 +0900

    deck

--- Mare_TestGame/monster.py
+++ Mare_TestGame/monster.py
@@ -1,18 +1,21 @@
-class  monster:
-    def  __init__(self,name,hp,power):
-        self.name  = name
-        self.hp  = hp
-        self.power  = power
+class monster:
+    def __init__(self, name, hp, power):
+        self.name = name
+        self.hp = hp
+        self.power = power
 
-monsters  = []
-class  field_mons_generator:
+
+monsters = []
+
+
+class field_mons_generator:
     def grasa_generator(self):
-        slime  = monster("slime",5,10)
-        orc  = monster("orc",15,50)
-        goblin = monster("goblin",20,20)
+        slime = monster("slime", 5, 10)
+        orc = monster("orc", 15, 50)
+        goblin = monster("goblin", 20, 20)
 
         monsters.append(slime)
         monsters.append(orc)
         monster.append(goblin)
 
-        return  monsters
+        return monsters
